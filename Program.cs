ca3ffd604eabe4172e670ec77dbae9ba1ed7dbca
ewurabenafilson1 <ewurabenafilson1@gmail.com>
2025-11-05 22:01:35 -0800

    student management system initial commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+class Program
+{
+    static void Main()
+    {
+        List<Student> studentsInfo = new List<Student>();
+        string file = "students.json";
+
+        if (File.Exists(file))
+        {
+            string json = File.ReadAllText(file);
+            studentsInfo = JsonSerializer.Deserialize<List<Student>>(json);
+        }
+
+        while (true)
+        {
+            Console.WriteLine("\n===== STUDENT MANAGEMENT SYSTEM =====");
+            Console.WriteLine("1. Add Student");
+            Console.WriteLine("2. View Students");
+            Console.WriteLine("3. Update Student");
+            Console.WriteLine("4. Delete Student");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    // TODO: Add student
+                    break;
+                case "2":
+                    // TODO: View students
+                    break;
+                case "3":
+                    // TODO: Update student
+                    break;
+                case "4":
+                    // TODO: Delete student
+                    break;
+                case "5":
+                    Console.WriteLine("Save and Exit");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    break;
+            }
+        }
+
+         static void AddStudent(List<Student> studentsInfo)
+    {
+        Console.Write("\nEnter index number: ");
+        string indexNumber = Console.ReadLine();
+
+        Console.Write("Enter name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Enter email: ");
+        string email = Console.ReadLine();
+        
+        Console.Write("Enter course: ");
+        string course = Console.ReadLine();
+        
+
+
+        Student student = new Student(indexNumber, name , age, email, course);
+        studentsInfo.Add(student);
+
+        Console.WriteLine("\n✅ Student added successfully!\n");
+    }
+
+
+
+    }
+}
---
+++ student.cs
@@ -0,0 +1,28 @@
+class Student
+{
+    public string IndexNumber{ get; set; }
+    public string  Name { get; set; }
+
+    public int Age { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string Course { get; set; }
+
+    public Student() { }
+    //for serialization and deserialization
+
+    public Student(string indexNumber, string name, int age, string email, string course)
+    {
+        IndexNumber = indexNumber;
+        Name = name;
+        Age = age;
+        Email = email;
+        Course = course;
+    }
+
+    public override string ToString()
+    {
+        return $"Index Number: {IndexNumber}| Name: {Name} | Age: {Age} | Email: {Email} | Course: {Course} ";
+    }
+}
